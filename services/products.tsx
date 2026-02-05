c9358c9fa5432b440beb53f5d513081501a73f19
Dalienst Owino <oduordalienst@gmail.com>
2026-02-05 12:19:55 +0300

    prepare for products

--- services/products.tsx
+++ services/products.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { apiActions } from "@/tools/axios";
+import { apiActions, apiMultipartActions } from "@/tools/axios";
 import { AxiosResponse } from "axios";
 import { PaginatedResponse } from "./general";
 
@@ -53,3 +53,89 @@ export interface Product {
     total_stock: number;
 }
 
+export interface createProduct {
+    name: string;
+    description: string;
+    sub_categories: string[]; // sub category name
+    tags: string[]; // tag names. As the vendor pleases
+    is_active: boolean;
+}
+
+export interface addProductImages {
+    uploaded_images: File[]; // can be multiple
+}
+
+export interface updateProduct {
+    name: string;
+    description: string;
+    sub_categories: string[]; // sub category name
+    tags: string[]; // tag names. As the vendor pleases
+    is_active: boolean;
+}
+
+export const getProductsVendor = async (headers: { headers: { Authorization: string } }): Promise<Product[]> => {
+    const response: AxiosResponse<PaginatedResponse<Product>> = await apiActions.get(
+        `/api/v1/products/`,
+        headers
+    );
+    return response.data.results || [];
+};
+
+export const getProductVendor = async (reference: string, headers: { headers: { Authorization: string } }): Promise<Product> => {
+    const response: AxiosResponse<Product> = await apiActions.get(
+        `/api/v1/products/${reference}/`,
+        headers
+    );
+    return response.data;
+};
+
+export const createProduct = async (data: createProduct, headers: { headers: { Authorization: string } }): Promise<Product> => {
+    const response: AxiosResponse<Product> = await apiActions.post(
+        `/api/v1/products/`,
+        data,
+        headers
+    );
+    return response.data;
+};
+
+export const addProductImages = async (reference: string, data: addProductImages, headers: { headers: { Authorization: string } }): Promise<Product> => {
+    const response: AxiosResponse<Product> = await apiMultipartActions.patch(
+        `/api/v1/products/${reference}/`,
+        data,
+        headers
+    );
+    return response.data;
+};
+
+export const updateProduct = async (reference: string, data: updateProduct, headers: { headers: { Authorization: string } }): Promise<Product> => {
+    const response: AxiosResponse<Product> = await apiActions.patch(
+        `/api/v1/products/${reference}/`,
+        data,
+        headers
+    );
+    return response.data;
+};
+
+export const deleteProduct = async (reference: string, headers: { headers: { Authorization: string } }): Promise<Product> => {
+    const response: AxiosResponse<Product> = await apiActions.delete(
+        `/api/v1/products/${reference}/`,
+        headers
+    );
+    return response.data;
+};
+
+// Public Views
+
+export const getProducts = async (): Promise<Product[]> => {
+    const response: AxiosResponse<PaginatedResponse<Product>> = await apiActions.get(
+        `/api/v1/products/`
+    );
+    return response.data.results || [];
+};
+
+export const getProduct = async (productcode: string): Promise<Product> => {
+    const response: AxiosResponse<Product> = await apiActions.get(
+        `/api/v1/products/${productcode}/`
+    );
+    return response.data;
+};
