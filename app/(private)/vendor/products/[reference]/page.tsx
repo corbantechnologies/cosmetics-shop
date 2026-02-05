7e56ff86520b9f85b0b055913ba733140c0158a9
Dalienst Owino <oduordalienst@gmail.com>
2026-02-05 12:38:40 +0300

    add workflo

---
+++ app/(private)/vendor/products/[reference]/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import SectionHeader from "@/components/dashboard/SectionHeader";
+
+export default function ProductPage() {
+    return (
+        <div className="min-h-screen bg-background">
+            <SectionHeader title="Product" description="Product Details" />
+        </div>
+    );
+}
